feceda3a02c70b74f4016a2a8cf8593bf7ddd034
Nicolas Pavon <pavonnicolas11@gmail.com>
2025-12-01 10:37:35 -0300

    Cambio rama

---
+++ cypress/e2e/search/filteredSearch.cy.js
@@ -0,0 +1,16 @@
+describe('Filtered Search',()=>{
+    // it("Filtered Search", () => {
+  //   cy.searchByProductName("cama");
+  //   cy.get('[data-test-id="price-filter-option"]').first().click();
+  //   cy.get(".sc-8f88aecf-2.bJrZPO")
+  //     .first()
+  //     .scrollIntoView()
+  //     .click({ force: true });
+  //   cy.get("#DeliveryTypeFilter-PICKUP").click();
+  //   cy.get('[data-test-id="discount-filter"]>a').last().click();
+  //   cy.url().should("contain", "menos-de");
+  //   cy.url().should("contain", "formas-de-pago=12-cuotas-sin-interes");
+  //   cy.url().should("contain", "tipo-de-entrega=retiro-en-sucursal");
+  //   cy.url().should("contain", "descuento=desde");
+  // });
+})
--- cypress/e2e/search/search.cy.js
+++ cypress/e2e/search/search.cy.js
@@ -8,26 +8,14 @@ describe("Search Bar Flow", () => {
   //   cy.searchByProductName("Plancha");
   //   cy.get(".sc-1fa74e6c-0.kUaLHc").first().should("contain", "Plancha");
   // });
-  // it("Filtered Search", () => {
-  //   cy.searchByProductName("cama");
-  //   cy.get('[data-test-id="price-filter-option"]').first().click();
-  //   cy.get(".sc-8f88aecf-2.bJrZPO")
-  //     .first()
-  //     .scrollIntoView()
-  //     .click({ force: true });
-  //   cy.get("#DeliveryTypeFilter-PICKUP").click();
-  //   cy.get('[data-test-id="discount-filter"]>a').last().click();
-  //   cy.url().should("contain", "menos-de");
-  //   cy.url().should("contain", "formas-de-pago=12-cuotas-sin-interes");
-  //   cy.url().should("contain", "tipo-de-entrega=retiro-en-sucursal");
-  //   cy.url().should("contain", "descuento=desde");
-  // });
+  
   // it("Search by SKU", () => {
   //   cy.get("[data-suggestion-index=0]").type("570653"); ///Valija 20 Pulgadas 13602 10kg Negro
   //   cy.get(".sc-ejfMa-d.iFcilT").click();
   //   cy.get(".sc-1fa74e6c-0.kUaLHc").click();
   //   cy.get(".sc-2e32950b-3.cYRTi").should("contain", "570653");
   // });
+
   // it("order by price high to low", () => {
   //   cy.searchByProductName("plancha");
   //   cy.get('[data-test-id="order-by-select"]').click();
@@ -46,6 +34,7 @@ describe("Search Bar Flow", () => {
   //     );
   //   });
   // });
+  
   // it("Pagination next page button", () => {
   //   cy.searchByProductName("cama");
   //   for (let i = 2; i < 7; i++) {
